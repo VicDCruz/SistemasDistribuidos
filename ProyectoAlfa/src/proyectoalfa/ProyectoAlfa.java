81bf53a60f2eff0b87cb75e544e37eea91bf822d
PaolaMejia <43660286+PaolaMejia@users.noreply.github.com>
2019-02-05 12:25:53 -0600

    Second commit

--- ProyectoAlfa/src/proyectoalfa/ProyectoAlfa.java
+++ ProyectoAlfa/src/proyectoalfa/ProyectoAlfa.java
@@ -16,7 +16,7 @@ public class ProyectoAlfa {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
+        System.out.println("Hola Vic");
     }
     
 }
