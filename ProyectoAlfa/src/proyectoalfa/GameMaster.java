459ea361bc4b52de6f3711d264cab3007343eece
PaolaMejia <43660286+PaolaMejia@users.noreply.github.com>
2019-02-06 15:55:52 -0600

    player&master

--- ProyectoAlfa/src/proyectoalfa/GameMaster.java
+++ ProyectoAlfa/src/proyectoalfa/GameMaster.java
@@ -5,44 +5,124 @@
  */
 package proyectoalfa;
 
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.SocketException;
 
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
+ * Cliente UDP (Multicast) Servidor TCP
  *
  * @author pmeji
  */
 public class GameMaster {
-        public static void main(String args[]){ 
-  	 
-	MulticastSocket s =null;
-   	 try {
-                
-                InetAddress group = InetAddress.getByName("228.5.6.7"); // destination multicast group 
-	    	s = new MulticastSocket(6789);
-	   	s.joinGroup(group); 
-                //s.setTimeToLive(10);
-                System.out.println("Messages' TTL (Time-To-Live): "+ s.getTimeToLive());
-                String myMessage="Hello";
-                byte [] m = myMessage.getBytes();
-	    	DatagramPacket messageOut = 
-			new DatagramPacket(m, m.length, group, 6789);
-	    	s.send(messageOut);
 
-	    	s.leaveGroup(group);		
- 	    }
-         catch (SocketException e){
-             System.out.println("Socket: " + e.getMessage());
-	 }
-         catch (IOException e){
-             System.out.println("IO: " + e.getMessage());
-         }
-	 finally {
-            if(s != null) s.close();
+    //Global variables
+    private int xCoor;
+    private int yCoor;
+    private int round;
+    private int[][] scoreBoard;
+    private int n = 3; //Number of wins per round to win
+    private int maxNumPlayers = 10;
+
+    public GameMaster() {
+        round = 0;
+        scoreBoard = new int[maxNumPlayers][n];
+    }
+
+    public void sendMonster(int x, int y, int round) {
+        MulticastSocket s = null;
+        try {
+            xCoor = x;
+            yCoor = y;
+
+            InetAddress group = InetAddress.getByName("228.5.6.7"); // destination multicast group 
+            s = new MulticastSocket(6789);
+            s.joinGroup(group);
+            //s.setTimeToLive(10);
+            System.out.println("Messages' TTL (Time-To-Live): " + s.getTimeToLive());
+
+            String myMessage = "(" + x + ", " + y + ") round = " + round;
+            byte[] m = myMessage.getBytes();
+            DatagramPacket messageOut
+                    = new DatagramPacket(m, m.length, group, 6789);
+            s.send(messageOut);
+
+            s.leaveGroup(group);
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+
+    }
+
+    public void recieveAnswer() {
+        try {
+            int serverPort = 7897;
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+            while (true) {
+                System.out.println("Waiting for messages...");
+                Socket clientSocket = listenSocket.accept();  // Listens for a connection to be made to this socket and accepts it. The method blocks until a connection is made. 
+                Connection c = new Connection(clientSocket);
+                c.start();
+            }
+        } catch (IOException e) {
+            System.out.println("Listen :" + e.getMessage());
+        }
+    }
+
+    public static void main(String args[]) {
+        GameMaster gm = new GameMaster();
+        gm.sendMonster(1, 2, 1);
+
+    }
+
+}
+
+class Connection extends Thread {
+
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
+
+    public Connection(Socket aClientSocket) {
+        try {
+            clientSocket = aClientSocket;
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        try {			                 // an echo server
+            String data = in.readUTF();
+            System.out.println("Message received from: " + clientSocket.getRemoteSocketAddress());
+            
+            out.writeUTF(data);
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
         }
     }
 }
--- ProyectoAlfa/src/proyectoalfa/Player.java
+++ ProyectoAlfa/src/proyectoalfa/Player.java
@@ -5,50 +5,102 @@
  */
 package proyectoalfa;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author VicDCruz
  */
 public class Player {
-	InetAddress group
-	MulticastSocket socket;
-	byte[] buffer
 
-	Player(String ipAdress, int socket) {
-		this.group = InetAddress.getByName(ipAdress);
-		this.socket = new MulticastSocket(socket);
-		this.socket.joinGroup(group);
-		this.buffer = new byte[1000];
-	}
+    InetAddress group;
+    MulticastSocket socket;
+    byte[] buffer;
 
-	public boolean receiveExampleMessage() {
-		try {
-			for(int i=0; i< 3; i++) {
-				System.out.println("Waiting for messages");
-				DatagramPacket messageIn = 
-					new DatagramPacket(this.buffer, this.buffer.length);
-				this.socket.receive(messageIn);
-				System.out.println("Message: " + new String(messageIn.getData())+ " from: "+ messageIn.getAddress());
-			}
-		} catch (SocketException e){
-			System.out.println("Socket: " + e.getMessage());
-		}
-		catch (IOException e){
-			System.out.println("IO: " + e.getMessage());
-		}
-		finally {
-			if(this.socket != null) s.close();
+    Player(String ipAdress, int socket) {
+        try {
+            this.group = InetAddress.getByName(ipAdress);
+            this.socket = new MulticastSocket(socket);
+            this.socket.joinGroup(group);
+            this.buffer = new byte[1000];
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
         }
-	}
+    }
 
-    public static void main(String args[]){ 
-			System.out.println("hola");
-    }		     
-                 // get messages from others in group
+    //UDP receiver
+    public boolean receiveMonster() {
+        MulticastSocket s = null;
+        try {
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Waiting for messages");
+                DatagramPacket messageIn
+                        = new DatagramPacket(this.buffer, this.buffer.length);
+                this.socket.receive(messageIn);
+                System.out.println("Message: " + new String(messageIn.getData()) + " from: " + messageIn.getAddress());
+            }
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        } finally {
+            if (this.socket != null) {
+                s.close();
+            }
+        }
+        return true;
+    }
+
+    //TCP Sender
+    public void sendAnswer(int x, int y) {
+        Socket s = null;
+        try {
+            int serverPort = 7896;
+
+            s = new Socket("localhost", serverPort);
+            //   s = new Socket("127.0.0.1", serverPort);    
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            DataOutputStream out
+                    = new DataOutputStream(s.getOutputStream());
+            out.writeUTF(""+x+","+y);        	// UTF is a string encoding 
+
+            String data = in.readUTF();
+            System.out.println("Received: " + data);
+        } catch (UnknownHostException e) {
+            System.out.println("Sock:" + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            if (s != null) {
+                try {
+                    s.close();
+                } catch (IOException e) {
+                    System.out.println("close:" + e.getMessage());
+                }
+            }
+        }
+    }
+
+
+public static void main(String args[]) {
+        System.out.println("Hello, I'm a player");
+    }
+    // get messages from others in group
 }
