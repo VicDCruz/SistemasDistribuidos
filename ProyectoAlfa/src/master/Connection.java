e4a3d3968ac2fbc3da6c876e4597636c3418a274
Victor Cruz <vicdan215@gmail.com>
2019-02-08 17:11:26 -0600

    Format changes

---
+++ ProyectoAlfa/src/master/Connection.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package master;
+
+class Connection extends Thread {
+
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
+
+    public Connection(Socket aClientSocket) {
+        try {
+            clientSocket = aClientSocket;
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        try {			                 // an echo server
+            String data = in.readUTF();
+            System.out.println("Message received from: " + clientSocket.getRemoteSocketAddress());
+
+            out.writeUTF(data);
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
--- ProyectoAlfa/src/master/GameMaster.java
+++ ProyectoAlfa/src/master/GameMaster.java
@@ -37,7 +37,7 @@ public class GameMaster implements Play {
     private int n = 3; //Number of wins per round to win
     private int maxNumPlayers = 10;
     
-    private String InetAddressNum  = "228.5.6.7";
+    private String inetAddressNum  = "228.5.6.7";
     private int socketGroup = 6789;
 
     public GameMaster() throws RemoteException {
@@ -82,7 +82,7 @@ public class GameMaster implements Play {
             xCoor = x;
             yCoor = y;
 
-            InetAddress group = InetAddress.getByName(InetAddressNum); // destination multicast group 
+            InetAddress group = InetAddress.getByName(inetAddressNum); // destination multicast group 
             s = new MulticastSocket(socketGroup);
             s.joinGroup(group);
             //s.setTimeToLive(10);
@@ -130,45 +130,8 @@ public class GameMaster implements Play {
 
     @Override
     public Credential login(String name) throws RemoteException {
-        Credential info = new Credential(InetAddressNum,socketGroup);
+        Credential info = new Credential(inetAddressNum,socketGroup);
         return info; //To change body of generated methods, choose Tools | Templates.
     }
 
-}
-
-class Connection extends Thread {
-
-    DataInputStream in;
-    DataOutputStream out;
-    Socket clientSocket;
-
-    public Connection(Socket aClientSocket) {
-        try {
-            clientSocket = aClientSocket;
-            in = new DataInputStream(clientSocket.getInputStream());
-            out = new DataOutputStream(clientSocket.getOutputStream());
-        } catch (IOException e) {
-            System.out.println("Connection:" + e.getMessage());
-        }
-    }
-
-    @Override
-    public void run() {
-        try {			                 // an echo server
-            String data = in.readUTF();
-            System.out.println("Message received from: " + clientSocket.getRemoteSocketAddress());
-
-            out.writeUTF(data);
-        } catch (EOFException e) {
-            System.out.println("EOF:" + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("IO:" + e.getMessage());
-        } finally {
-            try {
-                clientSocket.close();
-            } catch (IOException e) {
-                System.out.println(e);
-            }
-        }
-    }
 }
