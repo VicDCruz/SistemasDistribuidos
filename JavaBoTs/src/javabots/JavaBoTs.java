b2a207a31258d40f76d20fe6ce83acb509c6c7f5
PaolaMejia <43660286+PaolaMejia@users.noreply.github.com>
2019-02-11 15:43:49 -0600

    imports

---
+++ JavaBoTs/src/javabots/JavaBoTs.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javabots;
+
+/**
+ *
+ * @author pmeji
+ */
+public class JavaBoTs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
